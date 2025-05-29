63a9fc63fb45a2f4d3db725db6d7c169423e8550
Eduardo <raphael.nascimento.dev@gmail.com>
2025-05-29 11:13:45 -0400

    add animation banner

--- script.js
+++ script.js
@@ -38,6 +38,11 @@ $(document).ready(function(){
         duration:2000,
         distance: '20%'
     });
+        ScrollReveal().reveal('#banner',{
+        origin: 'right',
+        duration:2000,
+        distance: '20%'
+    });
 
         ScrollReveal().reveal('.dish',{
         origin: 'left',
