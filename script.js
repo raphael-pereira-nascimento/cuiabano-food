6c3ad52ee7dc3f24d4d226de626a0a052fd6f180
Eduardo <raphael.nascimento.dev@gmail.com>
2025-06-03 11:20:51 -0400

    add fix script.js

--- script.js
+++ script.js
@@ -1,64 +1,61 @@
-$(document).ready(function(){
-    $('mobile_btn').on('click', function(){
-        $('"mobile_menu').toggleClass('active');
-        $('"mobile_menu').find('i').toggleClass('fax-x');
+$(document).ready(function() {
+    $('#mobile_btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
     });
-
+  
     const sections = $('section');
     const navItems = $('.nav-item');
-
-    $(window).on('scrool', function() {
+  
+    $(window).on('scroll', function () { 
         const header = $('header');
-        const scroolPosition = $(window).scrollTop() - header.outerHeight();
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
         let activeSectionIndex = 0;
-        
-        if(scroolPosition <= 0){
+  
+        if (scrollPosition <= 0) {
             header.css('box-shadow', 'none');
         } else {
-            header.css('box-shadow', '5px 1px 5px rgba(0,0,0, 0.1)');
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
         }
-
+  
         sections.each(function(i) {
-            const section= $(this);
-            const sectionTop = section.offset().top - 96;
-            const sectionBottom = sectionTop + section.outerHeight();
-
-            if(scroolPosition >= sectionTop && scroolPosition < sectionBottom) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96; 
+            const sectionBottom = sectionTop+ section.outerHeight();
+  
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
                 activeSectionIndex = i;
                 return false;
             }
-        });
+        })
+  
         navItems.removeClass('active');
-        $(navItems[activeSectionIndex]).addClas('active');
+        $(navItems[activeSectionIndex]).addClass('active');
     });
-
+  
+    
     /** Animação */
-    ScrollReveal().reveal('#cta',{
+    ScrollReveal().reveal('#cta', {
         origin: 'left',
-        duration:2000,
+        duration: 2000,
         distance: '20%'
     });
-        ScrollReveal().reveal('#banner',{
+  
+    ScrollReveal().reveal('.dish', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+  
+    ScrollReveal().reveal('#testimonial_dish', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    })
+  
+    ScrollReveal().reveal('.feedback', {
         origin: 'right',
-        duration:2000,
+        duration: 1000,
         distance: '20%'
-    });
-
-        ScrollReveal().reveal('.dish',{
-        origin: 'left',
-        duration:2000,
-        distance: '20%'
-    });
-
-        ScrollReveal().reveal('#testimonial_dish',{
-        origin: 'left',
-        duration:2000,
-        distance: '20%'
-    });
-
-            ScrollReveal().reveal('.feedback',{
-        origin: 'right',
-        duration:2000,
-        distance: '20%'
-    });
+    })
 });
